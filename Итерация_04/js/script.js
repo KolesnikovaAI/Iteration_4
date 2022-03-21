84e6eba342f56e897050309622e70f5e63dd269b
Anastasiya Kolesnikova <97825341+KolesnikovaAI@users.noreply.github.com>
2022-03-21 09:59:52 +0300

    Add files via upload

---
+++ Итерация_04/js/script.js
@@ -0,0 +1,12 @@
+let a = 50;
+let b = 5;
+let sum = a + b;
+console.log(`Сумма a и b ${sum}`);
+let dif = a-b;
+console.log(`Разность a и b ${dif}`);
+let str1 = "Я учу ";
+let str2 = "JS!";
+let str = str1 + str2;
+console.log(`конкатенация ${str}`);
+alert(str.match(/\s\w/))
+
